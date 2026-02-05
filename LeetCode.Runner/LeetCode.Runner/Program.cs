1e103d2c253e01fa9a25618255b56c749549e425
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-04 22:21:32 -0600

    11. Container with Most Water - Two Pointer

---
+++ LeetCode.Runner/LeetCode.Runner/Program.cs
@@ -0,0 +1,8 @@
+﻿using LeetCode.Medium;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine(ContainerWithMostWater.MaxArea(new int[]{ 1, 4, 6, 5, 3, 7, 6, 2}));
+Console.WriteLine(ContainerWithMostWater.MaxAreaTwoPointers(new int[] { 1, 4, 6, 5, 3, 7, 6, 2 }));
+Console.ReadLine();
---
+++ LeetCode/LeetCode/Medium/11ContainerWithMostWater.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Medium
+{
+    public static class ContainerWithMostWater
+    {
+        public static int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int max = 0;
+
+            while (left < right)
+            {
+                var width = right - left;
+                max = Math.Max(max, width * Math.Min(height[left], height[right]));
+
+                if (height[left] >= height[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+
+            return max;
+        }
+    }
+}
