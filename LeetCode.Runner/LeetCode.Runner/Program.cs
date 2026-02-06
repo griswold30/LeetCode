bdd5d92e508b004e9b6d15fff2b5ebaf6c2ac260
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-05 21:30:27 -0600

    1. Two Sum - Easy

--- LeetCode.Runner/LeetCode.Runner/Program.cs
+++ LeetCode.Runner/LeetCode.Runner/Program.cs
@@ -1,8 +1,11 @@
 ﻿using LeetCode.Medium;
+using LeetCode.Easy;
+using System.Linq;
+
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-
-Console.WriteLine(ContainerWithMostWater.MaxArea(new int[]{ 1, 4, 6, 5, 3, 7, 6, 2}));
-Console.WriteLine(ContainerWithMostWater.MaxAreaTwoPointers(new int[] { 1, 4, 6, 5, 3, 7, 6, 2 }));
+//Console.WriteLine(ContainerWithMostWater.MaxArea(new int[] { 1, 4, 6, 5, 3, 7, 6, 2 }));
+TwoSum.TwoSumFunc([3,2,4], 6).ToList().ForEach((x) => Console.Write(x.ToString()));
 Console.ReadLine();
+
+
---
+++ LeetCode/LeetCode/Easy/1TwoSum.cs
@@ -0,0 +1,75 @@
+﻿namespace LeetCode.Easy
+{
+    public static class TwoSum
+    {
+        //one-pass
+        //O(n) time
+        //O(n) space
+        public static int[] TwoSumFunc(int[] nums, int target)
+        {
+            var seen = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var compliment = target - nums[i];
+                if (seen.ContainsKey(compliment))
+                {
+                    return [i, seen[compliment]];
+                }
+
+                seen[nums[i]] = i;
+            }
+
+            return [];
+        }
+
+        //two-pass
+        //O(n) time
+        //O(n) space
+        //public static int[] TwoSumFunc(int[] nums, int target)
+        //{
+        //    var seen = new Dictionary<int, int>();
+
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        seen[nums[i]] = i;
+        //    }
+
+        //    for (var i = 0; i < nums.Length; i++)
+        //    {
+        //        var compliment = target - nums[i];
+        //        if (seen.ContainsKey(compliment) && seen[compliment] != i)
+        //        {
+        //            return [i, seen[compliment]];
+        //        }
+        //    }
+        //    return [];
+        //}
+
+        //O(n^2) time
+        //O(n) space
+        //public static int[] TwoSumFunc(int[] nums, int target)
+        //{
+        //    Dictionary<int, int> seen = new Dictionary<int, int>();
+
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        if (seen.TryGetValue(target - nums[i], out var index))
+        //        {
+        //            return [i, index];
+        //        }
+
+        //        for (int j = i+1; j < nums.Length; j++)
+        //        {
+        //            if (nums[j] == target - nums[i])
+        //            {
+        //                return [i, j];
+        //            }
+        //        }
+
+        //        seen.TryAdd(nums[i], i);
+        //    }
+        //    return [];
+        //}
+    }
+}
