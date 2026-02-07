93b4ff52ce81e545287137b492f3557b30ef8cd8
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-07 16:22:47 -0600

    167. Two Sum 2 - Medium

--- LeetCode.Runner/LeetCode.Runner/Program.cs
+++ LeetCode.Runner/LeetCode.Runner/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 // See https://aka.ms/new-console-template for more information
 
 //Console.WriteLine(ContainerWithMostWater.MaxArea(new int[] { 1, 4, 6, 5, 3, 7, 6, 2 }));
-TwoSum.TwoSumFunc([3,2,4], 6).ToList().ForEach((x) => Console.Write(x.ToString()));
+//TwoSum.TwoSumFunc([3,2,4], 6).ToList().ForEach((x) => Console.Write(x.ToString()));
+
+TwoSum2.TwoSum([2, 7, 11, 15], 9).ToList().ForEach((x) => Console.Write(x.ToString()));
 Console.ReadLine();
 
 
---
+++ LeetCode/LeetCode/Medium/167TwoSum2.cs
@@ -0,0 +1,90 @@
+﻿namespace LeetCode.Medium
+{
+    public static class TwoSum2
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            var low = 0;
+            var high = numbers.Length - 1;
+
+            while (low < high)
+            {
+                var sum = numbers[low] + numbers[high];
+                if (sum == target) return [low + 1, high + 1];
+                if (sum < target) low++;
+                if (sum > target) high--;
+            }
+
+            return [];
+
+        }
+
+        //one pass
+        //O(N) Time
+        //O(N) Space
+        public static int[] TwoSum_OnePass(int[] numbers, int target)
+        {
+
+            var seen = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var compliment = target - numbers[i];
+
+                if (seen.ContainsKey(compliment)) return [seen[compliment] + 1, i + 1];
+
+                seen[numbers[i]] = i;
+            }
+            return [];
+        }
+
+        //TwoPass
+        //O(N) Time
+        //O(N) Space
+        public static int[] TwoSum_TwoPass(int[] numbers, int target)
+        {
+            var seen = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                seen[numbers[i]] = i;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var number = numbers[i];
+                var compliment = target - number;
+                if (seen.ContainsKey(compliment))
+                    return [i + 1, seen[compliment] + 1];
+            }
+
+            return [];
+        }
+        
+        //NestedLoop
+        //O(N^2) Time
+        //O(N) Space
+        public static int[] TwoSum_BruteForce(int[] numbers, int target)
+        {
+            var seen = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                var compliment = target - numbers[i];
+                if (seen.ContainsKey(compliment))
+                    return [i + 1, seen[compliment] + 1];
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] == compliment)
+                        return [i + 1, j + 1];
+                    else
+                    {
+                        seen[numbers[j]] = j;
+                        if (numbers[j] > compliment)
+                            break;
+                    }
+                }
+            }
+            return [];
+        }
+    }
+}
