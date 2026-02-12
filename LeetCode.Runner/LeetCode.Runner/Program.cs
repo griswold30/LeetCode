f1bac4b765668658dc1e587777d3de0b392e3928
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-11 22:52:12 -0600

    977 Squares Of Sorted Array Two Pointer Easy

--- LeetCode.Runner/LeetCode.Runner/Program.cs
+++ LeetCode.Runner/LeetCode.Runner/Program.cs
@@ -1,5 +1,4 @@
-﻿using LeetCode.Medium;
-using LeetCode.Easy;
+﻿using LeetCode.Easy;
 using System.Linq;
 
 // See https://aka.ms/new-console-template for more information
@@ -7,7 +6,7 @@ using System.Linq;
 //Console.WriteLine(ContainerWithMostWater.MaxArea(new int[] { 1, 4, 6, 5, 3, 7, 6, 2 }));
 //TwoSum.TwoSumFunc([3,2,4], 6).ToList().ForEach((x) => Console.Write(x.ToString()));
 //_15ThreeSum.ThreeSum([-1, 0, 1, 2, -1, -4]).ToList().ForEach((x) => x.ToList().ForEach(y => Console.Write(y.ToString() + ",")));
-Console.WriteLine(_ThreeSumClosest.ThreeSumClosest([10, 20, 30, 40, 50, 60, 70, 80, 90], 1));
+//Console.WriteLine(_ThreeSumClosest.ThreeSumClosest([10, 20, 30, 40, 50, 60, 70, 80, 90], 1));
+_977SquaresOfSortedArray.SortedSquares([-4, -1, 0, 3, 10]).ToList().ForEach(y => Console.Write(y.ToString() + ","));
 Console.ReadLine();
-
 
---
+++ LeetCode/LeetCode/Easy/977SquaresOfSortedArray.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Easy
+{
+    public static class _977SquaresOfSortedArray
+    {
+        //O(N) time
+        //O(N) Space
+        public static int[] SortedSquares(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            var result = new int[nums.Length];
+
+            for (int i = nums.Length -1 ; i >= 0; i--)
+            {
+                var leftSquare = nums[left] * nums[left];
+                var rightSquare = nums[right] * nums[right];
+
+                if (leftSquare > rightSquare)
+                {
+                    result[i] = leftSquare;
+                    left++;
+                }
+                else
+                {
+                    result[i] = rightSquare;
+                    right--;
+                }
+            }
+            
+            return result;
+        }
+    }
+}
