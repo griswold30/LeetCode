7dbe0f6ea023a304fa426986a43b73501fdaca93
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-11 22:15:14 -0600

    881 Boats to Save People - Medium

---
+++ LeetCode/LeetCode/Medium/881BoatsToSavePeople.cs
@@ -0,0 +1,68 @@
+﻿namespace LeetCode.Medium
+{
+    public static class _881BoatsToSavePeople
+    {
+
+        //O(N log N) Time
+        //O(1) Space
+        public static int NumRescueBoatsOptimized(int[] people, int limit)
+        {
+            var boats = 0;
+            var left = 0;
+            var right = people.Length - 1;
+
+            Array.Sort(people);
+
+            while (left <= right)
+            {
+                boats++;
+
+                if (people[left] + people[right] <= limit)
+                {
+                    left++;
+                }
+
+               right--;
+            }
+
+            return boats;
+        }
+
+        //O(N log N) Time
+        //O(1) Space
+        public static int NumRescueBoats(int[] people, int limit)
+        {
+            var boats = 0;
+            var left = 0;
+            var right = people.Length - 1;
+
+            Array.Sort(people);
+            while (right > 0 && people[right] == limit)
+            {
+                boats++;
+                right--;
+            }
+
+            while (left <= right)
+            {
+                boats++;
+
+                if (left == right)
+                {
+                    break;
+                }
+                if (people[left] + people[right] <= limit)
+                {
+                    left++;
+                    right--;
+                }
+                else if (people[left] + people[right] > limit)
+                {
+                    right--;
+                }
+            }
+
+            return boats;
+        }
+    }
+}
