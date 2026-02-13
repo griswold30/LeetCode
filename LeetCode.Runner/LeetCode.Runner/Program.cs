65105eaebd9cd7800b93da69a57728725b2d4712
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-12 21:24:20 -0600

    259 3Sum Smaller - Medium

--- LeetCode.Runner/LeetCode.Runner/Program.cs
+++ LeetCode.Runner/LeetCode.Runner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode.Easy;
+using LeetCode.Medium;
 using System.Linq;
 
 // See https://aka.ms/new-console-template for more information
@@ -7,6 +8,7 @@ using System.Linq;
 //TwoSum.TwoSumFunc([3,2,4], 6).ToList().ForEach((x) => Console.Write(x.ToString()));
 //_15ThreeSum.ThreeSum([-1, 0, 1, 2, -1, -4]).ToList().ForEach((x) => x.ToList().ForEach(y => Console.Write(y.ToString() + ",")));
 //Console.WriteLine(_ThreeSumClosest.ThreeSumClosest([10, 20, 30, 40, 50, 60, 70, 80, 90], 1));
-_977SquaresOfSortedArray.SortedSquares([-4, -1, 0, 3, 10]).ToList().ForEach(y => Console.Write(y.ToString() + ","));
+//_977SquaresOfSortedArray.SortedSquares([-4, -1, 0, 3, 10]).ToList().ForEach(y => Console.Write(y.ToString() + ","));
+Console.WriteLine(_2593SumSmaller.ThreeSumSmaller([-2, 0, 1, 3], 2));
 Console.ReadLine();
 
---
+++ LeetCode/LeetCode/Medium/2593SumSmaller.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Medium
+{
+    public static class _2593SumSmaller
+    {
+        //O(n^2) time
+        //O(n) space
+        public static int ThreeSumSmaller(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            var tripletCount = 0;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int left = i + 1;
+                int right = nums.Length - 1;
+
+                var compliment = target - nums[i];
+
+                while (left < right)
+                {
+
+                    if (nums[left] + nums[right] < compliment)
+                    {
+                        tripletCount += right - left;
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return tripletCount;
+        }
+    }
+}
