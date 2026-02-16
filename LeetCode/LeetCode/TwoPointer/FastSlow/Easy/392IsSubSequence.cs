69cf956b0c228920f7b691b2f716b3bcd4dee87f
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-15 20:44:01 -0600

    392 Is Subsequence, Easy, Two Pointer, Fast Slow

---
+++ LeetCode/LeetCode/TwoPointer/FastSlow/Easy/392IsSubSequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.TwoPointer.FastSlow.Easy
+{
+    public static class _392IsSubSequence
+    {
+        public static bool IsSubsequence(string s, string t)
+        {
+            int subLimit = s.Length;
+            int targetLimit = t.Length;
+
+            int subIndex = 0;
+            int targetIndex = 0;
+
+            while (subIndex < subLimit && targetIndex < targetLimit)
+            {
+                if (s[subIndex] == t[targetIndex])
+                {
+                    subIndex++;
+                }
+                targetIndex++;
+            }
+
+            return subLimit == subIndex;
+        }
+    }
+}
