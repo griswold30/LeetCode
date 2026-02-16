39daa8acc9f8cff51ef4bc7d016a6b38f8586ae2
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-15 22:04:34 -0600

    876 middle of linked list - easy - fast/slow

---
+++ LeetCode/LeetCode/TwoPointer/FixedSeparation/Easy/876MiddleOfLinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using LeetCode.Common;
+
+
+namespace LeetCode.TwoPointer.FixedSeparation.Easy
+{
+    public static class _876MiddleOfLinkedList
+    {
+        //O(N) space
+        //O(1) Time
+        public static ListNode MiddleNode(ListNode head)
+        {
+            var first = head;
+            var second = head;
+
+            while (first != null && first.next != null)
+            {
+                first = first.next.next;
+                second = second.next;
+            }
+
+            return second;
+        }
+}
