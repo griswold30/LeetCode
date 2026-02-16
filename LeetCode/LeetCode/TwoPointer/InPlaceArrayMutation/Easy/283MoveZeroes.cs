81f426fd6c81d186b2f56e1b634bb02ae96cbbda
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-16 11:54:12 -0600

    283 Move Zeroes - Easy - Two Pointer

---
+++ LeetCode/LeetCode/TwoPointer/InPlaceArrayMutation/Easy/283MoveZeroes.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.TwoPointer.InPlaceArrayMutation.Easy
+{
+    public static class _283MoveZeroes
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            int i = 0;
+            for (int j = 0; j < nums.Length && i < nums.Length; j++)
+            {
+                i = j + 1;
+                if (nums[j] == 0)
+                {
+                    while (i < nums.Length && nums[i] == 0)
+                    {
+                        i++;
+                    }
+                    if (i == nums.Length) return;
+                    nums[j] = nums[i];
+                    nums[i++] = 0;
+                }
+            }
+        }
+    }
+}
