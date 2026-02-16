ef4e37111d1ecbcd8cb4ebdf74ac7fa9c553c05b
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-15 22:04:54 -0600

    2095 Delete the middle of a linked list, fixed separation, medium

---
+++ LeetCode/LeetCode/TwoPointer/FixedSeparation/Medium/2095DeleteTheMiddleNodeOfALinkedList.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.TwoPointer.FixedSeparation.Medium
+{
+    public static  class _2095DeleteTheMiddleNodeOfALinkedList
+    {
+        //O(N) time
+        //O(1) space
+        public static ListNode DeleteMiddle(ListNode head)
+        {
+            if (head.next == null) return null;
+
+            var slow = head;
+            var fast = slow.next.next;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            slow.next = slow.next.next;
+
+            return head;
+        }
+    }
+
+
+}
