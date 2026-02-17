8e29c0979f225a0ce3dbbe47f9d707fd1ebfa6f0
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-16 21:05:46 -0600

    905 Sort Array By Parity - Easy - Two Pointer

---
+++ LeetCode/LeetCode/TwoPointer/InPlaceArrayMutation/Easy/905SortArrayByParity.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.TwoPointer.InPlaceArrayMutation.Easy
+{
+    public static class _905SortArrayByParity
+    {
+        public static int[] SortArrayByParity(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (nums[left] % 2 > nums[right] % 2)
+                {
+                    var temp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = temp;
+                }
+
+                if (nums[left] % 2 == 0) left++;
+                if (nums[right] % 2 == 1) right--;
+            }
+
+            return nums;
+        }
+    }
+}
