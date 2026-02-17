f458c33946e98e73292ea641390cfff01509b07d
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-16 20:37:17 -0600

    443 String Compression - Medium - Two Pointer

---
+++ LeetCode/LeetCode/TwoPointer/InPlaceArrayMutation/Medium/443StringCompression.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.TwoPointer.InPlaceArrayMutation.Medium
+{
+    public static class _443StringCompression
+    {
+        public static int Compress(char[] chars)
+        {
+            var count = 0;
+            char last = chars[0];
+            int j = 0;
+            int i = 0;
+
+            while (i < chars.Length)
+            {
+                count = 1;
+                while (i + count < chars.Length && chars[i + count] == chars[i])
+                {
+                    count++;
+                }
+                chars[j++] = chars[i];
+                if (count > 1)
+                {
+                    foreach (var c in count.ToString().ToCharArray())
+                    {
+                        chars[j++] = c;
+                    }
+                }
+
+                i += count;
+            }
+            return j;
+        }
+    }
+}
