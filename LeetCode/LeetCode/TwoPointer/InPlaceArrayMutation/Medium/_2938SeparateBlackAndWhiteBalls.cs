545c747f2cd7dc8a3a2f7761c218cea3aaa9019f
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-02-23 22:04:30 -0600

    2938 - Separate Black and White Balls - Medium - Two Pointer

---
+++ LeetCode/LeetCode/TwoPointer/InPlaceArrayMutation/Medium/_2938SeparateBlackAndWhiteBalls.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.TwoPointer.InPlaceArrayMutation.Medium
+{
+    public static class _2938SeparateBlackAndWhiteBalls
+    {
+        public static long MinimumSteps(string s)
+        {
+            var whiteInvariantThreshold = 0;
+            var totalSteps = 0L;
+
+            for (var i =0; i < s.Length; i++)
+            {
+                if (s[i] == '0')
+                {
+                    totalSteps += (i - whiteInvariantThreshold);
+                    whiteInvariantThreshold++;
+                }
+            }
+
+            return totalSteps;
+        }
+    }
+}
