ad21216550b5812cde4d7b74930513e2b46b84ba
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-03-04 21:12:47 -0600

    Add 56 - Merge Intervals - GreedyPatterns - Medium

---
+++ LeetCode/LeetCode/GreedyPatterns/IntervalMerging/56MergeIntervals.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.GreedyPatterns.IntervalMerging
+{
+    public class _56MergeIntervals
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            List<int[]> merged = new List<int[]>();
+            Array.Sort(intervals, (a, b) => a[0] - b[0]);
+
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                if (merged.Count == 0)
+                {
+                    merged.Add(new[] { intervals[i][0], intervals[i][1] });
+                    continue;
+                }
+
+                var lastMerged = merged[^1];
+
+                if (lastMerged[1] < intervals[i][0])
+                {
+                    merged.Add(new[] { intervals[i][0], intervals[i][1] });
+                }
+                else
+                {
+                    lastMerged[1] = Math.Max(lastMerged[1], intervals[i][1]);
+                }
+            }
+
+            return merged.ToArray();
+        }
+    }
+}
