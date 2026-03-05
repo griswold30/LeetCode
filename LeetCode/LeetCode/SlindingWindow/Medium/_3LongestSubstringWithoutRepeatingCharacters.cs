d30d8d4211a2a21e8d89dbb2becaa83945931ba6
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2026-03-04 22:55:46 -0600

    Adds 3 Longest Substring - Sliding Window - Medium

---
+++ LeetCode/LeetCode/SlindingWindow/Medium/_3LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.SlindingWindow.Medium
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            Dictionary<char, int> lastIndexOf = new Dictionary<char, int>();
+
+            int left = 0;
+            int maxLength = 0;
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                if (lastIndexOf.TryGetValue(s[right], out var index))
+                {
+                    left = Math.Max(left, index + 1);
+                }
+                lastIndexOf[s[right]] = right;
+
+                maxLength = Math.Max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
